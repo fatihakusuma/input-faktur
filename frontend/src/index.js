a23abb01744ed388f53c994d8b72c033311318bf
fatihakusuma <fatihakusumawardhani@gmail.com>
2025-07-06 21:48:33 +0700

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UploadFaktur from "./UploadFaktur";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <UploadFaktur />
+  </React.StrictMode>
+);
