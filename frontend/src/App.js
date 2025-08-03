9d06b6fc00d59df7d867cb52d1e978eb8cf93b57
fatihakusuma <fatihakusumawardhani@gmail.com>
2025-08-03 15:22:52 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UploadPage from './pages/UploadPage';
 import ResultPage from './pages/ResultPage';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<UploadPage />} />
-        <Route path="/result" element={<ResultPage />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UploadPage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
