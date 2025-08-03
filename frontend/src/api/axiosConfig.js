f54f38adff9eb000d966156a50587135c53db942
fatihakusuma <fatihakusumawardhani@gmail.com>
2025-08-03 11:41:14 +0700

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,14 +1,13 @@
-
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: process.env.NODE_ENV === 'production' 
     ? '/api'  // Untuk production di Vercel
-    : 'http://localhost:5000',  // Untuk development
+    : 'http://localhost:5000/api',  // Untuk development
   headers: {
-    'Content-Type': 'application/json',
-    // Tambahkan header lain jika diperlukan
+    'Content-Type': 'application/json'
   },
+  timeout: 10000
 });
 
 // Interceptor untuk menangani error global
