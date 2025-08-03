983ed3b6e40d816e17858aab3708e847ba6a63d0
fatihakusuma <fatihakusumawardhani@gmail.com>
2025-08-03 15:58:56 +0700

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,5 +6,6 @@ const apotekController = require('../controllers/apotekController');
 router.post('/process-invoice', geminiController.processInvoice);
 router.post('/submit-invoice', apotekController.submitInvoice);
 router.post('/compare-prices', apotekController.comparePrices);
+router.get('/pharmacies', apotekController.getPharmacies); // TAMBAHKAN BARIS INI
 
 module.exports = router;
