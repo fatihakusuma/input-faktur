2c84c3053cfa50fb85859b1387ecc7a8758e2299
Fatiha0 <fatihakusumawardani@gmail.com>
2025-08-10 20:40:00 +0700

    repaired index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,6 +30,7 @@ class ErrorBoundary extends React.Component {
   }
 }
 
+const basename = process.env.NODE_ENV === 'production' ? '/' : '/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
